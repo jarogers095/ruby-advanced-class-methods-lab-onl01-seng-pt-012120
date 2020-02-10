91d6b0956ebc4b4704545d0194ed837c6513562c
James Rogers <jarogers095@gmail.com>
2020-02-10 06:38:04 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -2,12 +2,32 @@ class Song
   attr_accessor :name, :artist_name
   @@all = []
 
-  def self.all
+  def self.all()
     @@all
   end
 
-  def save
+  def self.find_by_name(song_name)
+    return @@all.find(song_name)
+  end
+
+  def save()
     self.class.all << self
   end
+  
+  def create()
+    save()
+    return self
+  end
+  
+  def new_by_name(song_name)
+    @name = song_name
+  end
+  
+  def create_by_name(song_name)
+    @name = song_name
+    save()
+  end
+  
+  
 
 end
